a9cfa9bd4db8b06f4fbf06b97ff6c46da698d9c3
Barbu Mihai <mihaibbb10@gmail.com>
2023-05-13 02:33:54 +0300

    .

--- backend/controllers/doctor.js
+++ backend/controllers/doctor.js
@@ -1,11 +1,9 @@
-const bcrypt = require("bcryptjs");
 const db = require("../db");
 
 exports.createDoctor = async (req, res, next) => {
     const { username, password } = req.body;
     try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        await db.query("INSERT INTO ?? (username, password) VALUES (?, ?)", [`${process.env.DB_PREFIX}tech_for_life.doctors`, username, hashedPassword]);
+        await db.query("INSERT INTO ?? (username, password) VALUES (?, ?)", [`${process.env.DB_PREFIX}tech_for_life.doctors`, username, password]);
         res.status(200).json({ success: true });
     } catch (e) {
         console.log(e);
