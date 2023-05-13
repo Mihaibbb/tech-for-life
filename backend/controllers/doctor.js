75017d5353a9b66626cc843da9dc57e6c4bce760
Barbu Mihai <mihaibbb10@gmail.com>
2023-05-13 03:54:38 +0300

    .

--- backend/controllers/doctor.js
+++ backend/controllers/doctor.js
@@ -3,7 +3,7 @@ const db = require("../db");
 exports.createDoctor = async (req, res, next) => {
     const { username, password } = req.body;
     try {
-        const otherUsers = await db.query("SELECT * FROM ?? WHERE name = ?", [`${process.env.DB_PREFIX}tech_for_life.doctors`, username]);
+        const otherUsers = await db.query("SELECT * FROM ?? WHERE username = ?", [`${process.env.DB_PREFIX}tech_for_life.doctors`, username]);
         if (otherUsers.length) return res.status(300).json({ success: false, message: "Doctor already exists!" });
         await db.query("INSERT INTO ?? (username, password) VALUES (?, ?)", [`${process.env.DB_PREFIX}tech_for_life.doctors`, username, password]);
         res.status(200).json({ success: true });
